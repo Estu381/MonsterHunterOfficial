edcc42626fabf9d157629c08810a574321fca6f8
ESTU PRAYOGA <prayogaestu628@gmail.com>
2023-11-27 09:38:48 +0700

    delete appbar on main

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
-class AboutPage extends StatelessWidget {
+class AnimatedBackground extends StatefulWidget {
+  final Widget child;
+
+  AnimatedBackground({required this.child});
+
+  @override
+  _AnimatedBackgroundState createState() => _AnimatedBackgroundState();
+}
+
+class _AnimatedBackgroundState extends State<AnimatedBackground> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Tentang Kami'),
-      ),
-      body: Center(
-        child: Text(
-          'Monster Hunter adalah sebuah game aksi RPG yang dikembangkan oleh Capcom.',
-          style: TextStyle(fontSize: 18),
-          textAlign: TextAlign.center, // Menambahkan textAlign untuk memusatkan teks
-        ),
-      ),
+    return CachedNetworkImage(
+      imageUrl: 'URL_GAMBAR_ANIMASI', // Ganti dengan URL gambar animasi Anda
+      placeholder: (context, url) => Container(color: Colors.transparent), // Placeholder sementara
+      errorWidget: (context, url, error) => Icon(Icons.error), // Widget yang ditampilkan jika terjadi kesalahan
+      imageBuilder: (context, imageProvider) {
+        return Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: imageProvider,
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: widget.child,
+        );
+      },
     );
   }
 }
