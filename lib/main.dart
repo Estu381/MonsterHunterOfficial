71233fdb1e2c8c9e294855fdcc307c3b39863916
ESTU PRAYOGA <prayogaestu628@gmail.com>
2023-11-27 20:01:58 +0700

    create image list in main

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,17 @@ class _HomePageState extends State<HomePage> {
     'assets/slide8.png',
   ];
 
+  final List<String> gameImages = [
+    'assets/mhn.png',
+    'assets/mhgu.png',
+    'assets/mhw.png',
+    'assets/mhwi.png',
+    'assets/rise.png',
+    'assets/rise2.png',
+    'assets/stories.png',
+    // ... tambahkan path gambar game yang lain sesuai kebutuhan
+  ];
+
   late PageController _pageController;
   int _currentPage = 0;
 
@@ -101,51 +112,89 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          // Slideshow menggunakan PageView
-          Container(
-            height: 200.0,
-            color: Colors.purple[100], // Warna latar belakang purple light
-            child: PageView.builder(
-              controller: _pageController,
-              itemCount: slideImages.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  width: MediaQuery.of(context).size.width, // Lebar layar
-                  margin: EdgeInsets.symmetric(horizontal: 5.0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.zero,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Slideshow menggunakan PageView
+            Container(
+              height: 185.0,
+              color: Colors.purple[100], // Warna latar belakang purple light
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: slideImages.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width, // Lebar layar
+                    margin: EdgeInsets.symmetric(horizontal: 0.0),
                     child: Image.asset(
                       slideImages[index],
                       fit: BoxFit.cover,
                     ),
-                  ),
-                );
-              },
-              onPageChanged: (index) {
-                setState(() {
-                  _currentPage = index;
-                });
-              },
+                  );
+                },
+                onPageChanged: (index) {
+                  setState(() {
+                    _currentPage = index;
+                  });
+                },
+              ),
             ),
-          ),
-          // Konten lainnya di bawah slideshow
-          Container(
-            color: Colors.black,
-            height: 200,
-            // Gantilah dengan konten atau widget lain yang diinginkan
-            child: Center(
-              child: Text(
-                '', // Kosongkan teks
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24.0,
+            // Teks "OUR GAMES" di luar PageView pada latar belakang hitam
+            Container(
+              color: Colors.black,
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Center(
+                child: Text(
+                  '**OUR GAMES**',
+                  style: TextStyle(
+                    color: Colors.white, // Warna teks putih
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            // GridView untuk menampilkan gambar game dengan lebar 1/3 dari lebar layar
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 16.0,
+                  mainAxisSpacing: 16.0,
+                ),
+                itemCount: gameImages.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width / 3 - 16.0,
+                    height: MediaQuery.of(context).size.width / 3 - 16.0,
+                    child: Image.asset(
+                      gameImages[index],
+                      fit: BoxFit.contain,
+                    ),
+                  );
+                },
+              ),
+            ),
+            // Konten lainnya di bawah slideshow
+            Container(
+              color: Colors.black,
+              // Gantilah dengan konten atau widget lain yang diinginkan
+              child: Center(
+                child: Text(
+                  '', // Kosongkan teks
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
