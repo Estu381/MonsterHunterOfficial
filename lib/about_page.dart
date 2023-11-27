bfa61ad0599879099ba673c8808d49a4bd002300
ESTU PRAYOGA <prayogaestu628@gmail.com>
2023-11-27 09:59:11 +0700

    change background color aboutpage

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -1,33 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
-class AnimatedBackground extends StatefulWidget {
-  final Widget child;
-
-  AnimatedBackground({required this.child});
-
-  @override
-  _AnimatedBackgroundState createState() => _AnimatedBackgroundState();
-}
-
-class _AnimatedBackgroundState extends State<AnimatedBackground> {
+class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CachedNetworkImage(
-      imageUrl: 'URL_GAMBAR_ANIMASI', // Ganti dengan URL gambar animasi Anda
-      placeholder: (context, url) => Container(color: Colors.transparent), // Placeholder sementara
-      errorWidget: (context, url, error) => Icon(Icons.error), // Widget yang ditampilkan jika terjadi kesalahan
-      imageBuilder: (context, imageProvider) {
-        return Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: imageProvider,
-              fit: BoxFit.cover,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tentang Kami'),
+      ),
+      body: Container(
+        color: Colors.black, // Set warna background menjadi hitam
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 20), // Jarak atas untuk teks
+            Center(
+              child: Text(
+                'Monster Hunter adalah sebuah game aksi RPG yang dikembangkan oleh Capcom.',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-          child: widget.child,
-        );
-      },
+          ],
+        ),
+      ),
     );
   }
 }
